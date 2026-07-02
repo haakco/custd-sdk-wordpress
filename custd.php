592477f5ae83cbbd2052eacf9f9ef3c3af803738
Tim Haak <tim@haak.co>
2026-07-02 19:28:57 +0200

    chore: release v1.5.7

--- custd.php
+++ custd.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Custd
  * Description: Sends redacted WordPress product and activity events to Custd through the shared PHP SDK.
- * Version: 1.5.6
+ * Version: 1.5.7
  * Author: HaakCo
  * License: MIT
  */
