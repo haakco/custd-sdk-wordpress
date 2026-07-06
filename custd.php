6f479ed8a98b586dbf1fa90c0867ba56046f9c2f
Tim Haak <tim@haak.co>
2026-07-07 00:49:56 +0200

    chore(release): add version bump script and prepare v1.5.12

--- custd.php
+++ custd.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Custd
  * Description: Sends redacted WordPress product and activity events to Custd through the shared PHP SDK.
- * Version: 1.5.11
+ * Version: 1.5.12
  * Author: HaakCo
  * License: MIT
  */
