9920eb8dc314448c450163151b66a868e04286f8
custd-sdk-ci <ci@haak.co>
2026-08-01 01:39:17 +0200

    chore: publish wordpress-plugin subtree

--- custd.php
+++ custd.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Custd
  * Description: Sends redacted WordPress product and activity events to Custd through the shared PHP SDK.
- * Version: 1.8.1
+ * Version: 1.8.4
  * Author: HaakCo
  * License: MIT
  */
